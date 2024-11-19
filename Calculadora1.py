57aa4315829b03af0978207bb93ed88520398236
Laura0504t <ltapias.mesa@gmail.com>
2024-11-19 13:12:23 -0800

    Add files via upload

---
+++ Calculadora1.py
@@ -0,0 +1,14 @@
+print("Calculadora")
+print()
+
+numero_uno = int(input("Ingresa el primer numero:"))
+numero_dos = int(input("Ingrese el segundo numero:"))
+suma = numero_uno + numero_dos
+resta = numero_uno - numero_dos
+multiplicacion = numero_uno + numero_dos 
+division = numero_uno // numero_dos
+
+print("El resultado de la suma es:", suma)
+print(" El resultado de la resta es: ", resta)
+print("El resultado de la multiplicacion es:", multiplicacion )
+print("El resultado de la division es:", division )
